bfd912ba99bf824182ffe510f4058064a31a40cf
ImranRahimov1995 <imash.odessahouse@gmail.com>
2023-05-26 12:57:49 +0400

    add redis

--- project/config/settings/production.py
+++ project/config/settings/production.py
@@ -12,10 +12,10 @@ DATABASES = {
 }
 
 
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 # Redis settings
-REDIS_HOST = 'localhost'
+REDIS_HOST = 'redis'
 REDIS_PORT = 6379
 REDIS_DB = 1
